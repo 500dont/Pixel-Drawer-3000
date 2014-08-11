8f82e1b17aae0cc439b75e092907992233a6a197
Mady Mellor <madym@Madys-MacBook-Pro.local>
2014-08-10 23:01:33 -0700

    Initial Commit

---
+++ LearnSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LearnSwift
+//
+//  Created by Mady Mellor on 8/10/14.
+//  Copyright (c) 2014 Mady Mellor. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LearnSwift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  LearnSwift
+//
+//  Created by Mady Mellor on 8/10/14.
+//  Copyright (c) 2014 Mady Mellor. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
